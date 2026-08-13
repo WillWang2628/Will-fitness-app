4cae22f68c7bf3dab6e0d650c69138f7592ff729
Will Wang <superwin1126@gmail.com>
2026-08-13 14:18:46 +0800

    Rename app to Will's健身訓練菜單

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 之後把 Google Sheet「發布到網路」的 CSV 連結貼在這裡，App 就會改讀 Sheet 而不是內建資料。
 // 留空 = 用 data.js 裡的內建資料。
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTRM0CKFwC3SArRDaOrhRNnJNHc44TwT7vN_6xxJgdberrLl8XnSYAl20Tg9FLLJ7prrfNou0hGsPTL/pub?output=csv";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQUmEMQ81ZpNThRc-YWffVCSLzB2wr4RtZNnOC7bPhCdVLjupFJJBB-gLtqKRC_UB_aFtEV080Ru1Hw/pub?gid=1451736257&single=true&output=csv";
